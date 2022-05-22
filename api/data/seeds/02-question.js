8112711e5c2fb3f7e5eeadfe7f917151f964d24c
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-05-22 17:14:46 -0400

    created seed for questions --> FAQ

---
+++ api/data/seeds/02-question.js
@@ -0,0 +1,34 @@
+exports.seed = function (knex) {
+  return knex('question').insert([
+    {
+      question: 'How do you get the code for the bank vault?',
+      answer: 'You checkout their branch.',
+      reason: null,
+      archived: false,
+    },
+    {
+      question: 'What do you call a busy waiter?',
+      answer: 'A server.',
+      reason: null,
+      archived: false,
+    },
+    {
+      question: 'What do you call an idle server?',
+      answer: 'A waiter.',
+      reason: null,
+      archived: false,
+    },
+    {
+      question: 'What diet did the ghost developer go on?',
+      answer: 'Boolean.',
+      reason: null,
+      archived: false,
+    },
+    {
+      question: 'Why was the developer unhappy at their job?',
+      answer: 'They wanted arrays.',
+      reason: null,
+      archived: false,
+    },
+  ]);
+};
