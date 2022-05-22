04217b55ea7b64f31913a3d69f114248e2663602
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-05-22 17:18:43 -0400

    cleaned up server.js

--- api/server.js
+++ api/server.js
@@ -1,22 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-// const db = require('./data/db-config');
-
-// function getAllUsers() {
-//   return db('users');
-// }
-
-// async function insertUser(user) {
-//   // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
-//   // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
-//   const [newUserObject] = await db('users').insert(user, [
-//     'user_id',
-//     'username',
-//     'password',
-//   ]);
-//   return newUserObject; // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
-// }
 
 const server = express();
 server.use(express.json());
@@ -26,13 +10,5 @@ server.use(cors());
 server.get('/', async (req, res) => {
   res.status(201).json({ message: 'shariq was here' });
 });
-
-// server.get('/api/users', async (req, res) => {
-//   res.json(await getAllUsers());
-// });
-
-// server.post('/api/users', async (req, res) => {
-//   res.status(201).json(await insertUser(req.body));
-// });
 
 module.exports = server;
