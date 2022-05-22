11e7c4f03987e5eda0590a8799485bcc7f831721
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-05-22 17:03:56 -0400

    created migrations for table

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,13 +1,15 @@
+const maxStringLength = 200;
+
 exports.up = async (knex) => {
-  await knex.schema
-    .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('username', 200).notNullable()
-      users.string('password', 200).notNullable()
-      users.timestamps(false, true)
-    })
-}
+  await knex.schema.createTable('questions', (tbl) => {
+    tbl.increments('id');
+    tbl.string('question', maxStringLength).notNullable().unique();
+    tbl.string('answer', maxStringLength);
+    tbl.string('reason', maxStringLength);
+    tbl.boolean('archived').notNullable().defaultTo(false);
+  });
+};
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
-}
+  await knex.schema.dropTableIfExists('questions');
+};
